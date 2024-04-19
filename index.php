f58b192bcd9817848204fed38aaa3877feede47a
nhitl <101308283+nhitl@users.noreply.github.com>
2024-04-19 07:40:50 +0000

    NhiCommit

--- index.php
+++ index.php
@@ -25,6 +25,27 @@ function sayHello($name) {
 			echo "$p </br>";
 			echo $s;
 			//ok
+			
+			$n = 100;
+							$arr1 = array($n);
+							$arr2 = array(1,2,3,4);
+							$arr3 = array();
+							$arr4 = array("hoten" => "Trần Linh Nhi",
+										"quequan" => "Ha Nội",
+										"tuoi" => 22			
+							);
+							print_r($n);
+							print_r($arr1);
+							print_r($arr2);
+							print_r($arr3);
+							print_r($arr4);
+		
+		for($i=1;$i<=200;$i++){
+			if($i%2==0)
+				echo "<b><font color=red>".$i."</font></b>";
+		else
+				echo "<i><font color=blue>".$i."</font></i>";
+		}
 		?>
 	</body>
 </html>
